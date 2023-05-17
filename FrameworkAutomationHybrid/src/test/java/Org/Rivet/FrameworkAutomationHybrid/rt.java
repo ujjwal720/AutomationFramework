8313c77c3a54a64befaf87d6a65e90f64125b137
ujjwal.shrivastava <ujjwal.shrivastava@LP95.1rivet.local>
2023-05-17 14:36:51 +0530

    Commit the following in the git hub branch.

---
+++ FrameworkAutomationHybrid/src/test/java/Org/Rivet/FrameworkAutomationHybrid/rt.java
@@ -0,0 +1,20 @@
+package Org.Rivet.FrameworkAutomationHybrid;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class rt {
+	
+	
+	
+	@Test
+	public void msk() {
+		
+		
+		
+		
+		
+	}
+
+}
